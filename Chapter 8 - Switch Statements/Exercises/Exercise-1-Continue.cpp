3e5365cb1ad076c681decb79ea580dda3109000f
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-10-22 14:38:44 +0400

    Add some spaces and curly brackets to each case to organize the program better

--- Chapter 8 - Switch Statements/Exercises/Exercise-1-Continue.cpp
+++ Chapter 8 - Switch Statements/Exercises/Exercise-1-Continue.cpp
@@ -3,31 +3,37 @@ using namespace std;
 
 int main()
 {
-    char input;
-    cout << "Would you like to continue playing the game? Type Y for yes, or N for no\n";
-    cin >> input;
+    char input; 
+    cout << "Would you like to continue playing the game?\n"; 
+    cout << "\nPlease type Y if yes, or N for no.\n";  
+    cin >> input; 
 
-    switch (input) {
+    switch (input) 
+    {
 
-    case 'Y':
-    case 'y':
+    case 'Y': 
+    case 'y': 
+    {   
+        cout << "You have chosen yes.\n"; 
+        cout << "Continuing the game...\n"; 
 
-        cout << "Continuing the game...\n";
-        break;
-
+        break; 
+    }
     case 'N':
-    case 'n':
+    case 'n': 
+    {
+        cout << "You have chosen no.\n"; 
+        cout << "Stopping the game...\n"; 
 
-        cout << "Stopping the game...\n";
-        break;
-
-    default:
-        cout << "Wrong input\n";
-
+        break; 
+    }
+    default: 
+    {
+        cout << "Incorrect input\n"; 
+        cout << "Please type either Y or N.\n"; 
 
     }
     cin.get();
     return 0;
 }
-
 
