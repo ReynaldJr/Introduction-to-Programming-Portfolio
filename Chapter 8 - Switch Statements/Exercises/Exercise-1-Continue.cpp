5686851eff7f6ff59b9cae3e00b8173185418ed6
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-10-22 14:40:33 +0400

    Add comments to lines to make user understand the program better

--- Chapter 8 - Switch Statements/Exercises/Exercise-1-Continue.cpp
+++ Chapter 8 - Switch Statements/Exercises/Exercise-1-Continue.cpp
@@ -3,37 +3,38 @@ using namespace std;
 
 int main()
 {
-    char input; 
-    cout << "Would you like to continue playing the game?\n"; 
-    cout << "\nPlease type Y if yes, or N for no.\n";  
-    cin >> input; 
+    char input; //Declaring the variable for input.
+    cout << "Would you like to continue playing the game?\n"; //Prints the message that asks the user for input.
+    cout << "\nPlease type Y if yes, or N for no.\n";  //Prints the instructions of what the user should input
+    cin >> input; // Gets the user input.
 
-    switch (input) 
+    switch (input) { //Evaluating the expression using switch.
+
+    case 'Y': // Case for uppercase Y.
+    case 'y': // Case for lowercase y.
     {
+        cout << "You have chosen yes.\n"; //Prints message if user inputs Y.
+        cout << "Continuing the game...\n"; //Prints message if user inputs Y.
 
-    case 'Y': 
-    case 'y': 
-    {   
-        cout << "You have chosen yes.\n"; 
-        cout << "Continuing the game...\n"; 
-
-        break; 
+        break; //Ends the case
     }
-    case 'N':
-    case 'n': 
+    case 'N': // Case for uppercase N.
+    case 'n': // Case for lowercase n.
     {
-        cout << "You have chosen no.\n"; 
-        cout << "Stopping the game...\n"; 
+        cout << "You have chosen no.\n"; //Prints message if user inputs N.
+        cout << "Stopping the game...\n"; //Prints message if user inputs N.
 
-        break; 
+        break; //Ends the case
     }
-    default: 
+    default: //Default case
+
     {
-        cout << "Incorrect input\n"; 
-        cout << "Please type either Y or N.\n"; 
+        cout << "Incorrect input\n"; //Prints message if user inputs neither Y or N.
+        cout << "Please type either Y or N.\n"; //Prints message if user inputs neither Y or N.
+    }
 
     }
-    cin.get();
+    cin.get(); // Keeps the console window open.
     return 0;
 }
 
