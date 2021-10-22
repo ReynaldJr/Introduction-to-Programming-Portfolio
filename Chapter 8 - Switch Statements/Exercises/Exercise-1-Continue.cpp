43c9bf844367d638cf27bbadf9d0dfe7c3cdd15e
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-10-22 14:32:27 +0400

    Add the actual contents for the file

--- Chapter 8 - Switch Statements/Exercises/Exercise-1-Continue.cpp
+++ Chapter 8 - Switch Statements/Exercises/Exercise-1-Continue.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char input;
+    cout << "Would you like to continue playing the game? Type Y for yes, or N for no\n";
+    cin >> input;
+
+    switch (input) {
+
+    case 'Y':
+    case 'y':
+
+        cout << "Continuing the game...\n";
+        break;
+
+    case 'N':
+    case 'n':
+
+        cout << "Stopping the game...\n";
+        break;
+
+    default:
+        cout << "Wrong input\n";
+
+
+    }
+    cin.get();
+    return 0;
+}
+
 
