34dc8b13bd2cf99ee70f8a751c23cad5b0a00e44
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-10-23 17:28:51 +0400

    Add options to simplify user input

--- Chapter 7 - Compound Conditions/Exercises/Exercise-4-Starting-a-Band.cpp
+++ Chapter 7 - Compound Conditions/Exercises/Exercise-4-Starting-a-Band.cpp
@@ -5,42 +5,60 @@ using namespace std;
 int main() {
 
 	cout << "This Program Will Check If Your Friend Is Eligible to Enter the Band and If You Can Start a Band!\n\n"; // Outputs the title of the program
-	bool musicalFriend = true;
-	string friendPlays = "guitar";
-	string friendPlays2 = "drums";
+	bool musicalFriend; // Declaring variable for musicalFriend
+	string friendPlays; // Declaring variable for the instrument the friend plays
+	string input; // Declaring variable for user input
 
-	bool musicalFriendin;
-	string friendPlaysin;
-
-	cout << "Is your friend talented at music? (Write 0 if No, Write 1 if Yes)\n"; // Asks the user if the friend is talented at music
-	cin >> musicalFriendin; // User inputs answer
+	cout << "Does your friend play any instrument for music? (Write N if No, Write Y if Yes)\n"; // Asks the user if the friend is talented at music
+	cin >> input; // User inputs answer
 
 
-	if (musicalFriend == musicalFriendin) { // If the user is a musical friend this will execute
+	if (input == "Y" || input == "y") {
 
-		cout << "What instrument does your friend play? (Please type in lowercase only)\n"; // Asks the user what instrument the friend plays
-		cin >> friendPlaysin; // User inputs instrument
+		musicalFriend = true;
 
-		if (friendPlaysin == friendPlays || friendPlaysin == friendPlays2) { // If the user's friend plays guitar or drums this will execute
 
-			cout << "\nCongratulations, your friend can enter the band!\n";
-			cout << "You can start a band!\n";
+		if (musicalFriend == true) { // If the user's friend plays the instrument this will execute
+
+			cout << "What instrument does your friend play? (Please follow the instructions below)\n\n"; // Asks the user what instrument the friend plays
+			cout << "- Please type 'G' if your friend plays the guitar.\n- Please type 'D' if your friend plays the drums.\n- Please type 'O' if your friend plays other instruments.\n"; // Outputs the instructions for user input
+			cin >> friendPlays; // User inputs instrument
+
+			if (friendPlays == "G" || friendPlays == "g") { // If the user's friend plays the guitar this will execute
+
+				cout << "\nCongratulations, you are actually looking for a guitarist! Your friend can enter the band.\n";
+				cout << "You can start a band!\n";
+
+			}
+			else if (friendPlays == "D" || friendPlays == "d") { // If the user's friend plays the drums this will execute
+
+				cout << "\nCongratulations, you are actually looking for a drummer! Your friend can enter the band.\n";
+				cout << "You can start a band!\n";
+
+			}
+			else if (friendPlays == "O" || friendPlays == "o") { // If the user types the input incorrectly or if the user's friend plays an instrument other than guitar or drums this will execute
+
+				cout << "\nAww... Sorry, we don't accept the instrument your friend is using for the band. Try looking for someone else.\n";
+				cout << "You can't start a band.\n";
+			}
+
+			else { // If user doesn't type what is said in the instructions correctly, this will output.
+				cout << "Invalid Input\n";
+			}
 
 		}
-
-		else { // If the user types the input incorrectly or if the user's friend plays an instrument other than guitar or drums this will execute
-
-			cout << "\nAww... Sorry, we don't accept the instrument your friend is using for the band.\n"; 
-			cout << "You can't start a band.\n";
-		}
-
 	}
 
-	else { // If the user is not a musical friend this will execute
+	else if (input == "N" || input == "n") { // If the user is not a musical friend this will execute
 		
-		cout << "\nAww... Sorry, we only allow musical friends in the band.\n";
+		cout << "\nAww... Sorry, you are only looking for friends that can play the instrument. Try looking for someone else.\n";
 		cout << "You can't start a band.\n";
 	}
-	cin.get();
+
+	else { // If user doesn't type what is said in the instructions correctly, this will output.
+		cout << "Invalid Input\n";
+	}
+
+	cin.get(); // Keeps the console window open
 	return 0;
 }
