6a31f8f5127008aba9a4d75e735455dbb9c51e2a
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-10-28 19:00:07 +0400

    Create Exercise-1-Name.cpp

---
+++ Chapter 3 - Getting Started With C++/Exercises/Exercise-1-Name.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "Name: Reynald P. Padilla Jr." << endl; //Prints my name. 
+	cout << "Age: 17" << endl; //Prints my age. 
+	cout << "Current Address: Nasser Al Shaiba Building, Al Nahda Street, Sharjah, United Arab Emirates" << endl;    //Prints my current address.
+	cout << "Permanent Address: Nasser Al Shaiba Building, Al Nahda Street, Sharjah, United Arab Emirates" << endl;    //Prints my permanent address.
+
+	return 0;
+
+
+}
