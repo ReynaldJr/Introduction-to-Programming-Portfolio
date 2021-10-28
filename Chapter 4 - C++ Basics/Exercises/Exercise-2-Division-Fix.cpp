42d03cfe4d0caf8688ff3efe94abe8c9f84a646a
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-10-28 20:02:42 +0400

    Create Exercise-2-Division-Fix.cpp

---
+++ Chapter 4 - C++ Basics/Exercises/Exercise-2-Division-Fix.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	double numberOne = 50; // Changed the declaration from int to double since the answer contains decimals.
+	double numberTwo = 7; // Changed the declaration from int to double since the answer contains decimals.
+	
+	cout << numberOne / numberTwo << endl; // Outputs the answer
+
+	cin.get(); // Keeps the console window
+
+	return 0;
+
+
+}
