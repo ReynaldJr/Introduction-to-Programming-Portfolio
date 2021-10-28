f7ad3868f19c2e725950670ed332f58792180e47
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-10-28 20:24:50 +0400

    Add additional comments to make user understand the program better

--- Chapter 4 - C++ Basics/Exercises/Exercise-5-Declaration-and-Initialisation.cpp
+++ Chapter 4 - C++ Basics/Exercises/Exercise-5-Declaration-and-Initialisation.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int  a = 8; // Declaring an int variable with a value of 8 as an initialsation
     int b = 10; // Declaring an int variable with a value of 10 as an initialisation
-    int sum; // Declaring an int variable that sums the answer
+    int sum; // Declaring an int variable that sums the answer between variables a and b
 
     sum = a + b; // Formula for the sum
 
