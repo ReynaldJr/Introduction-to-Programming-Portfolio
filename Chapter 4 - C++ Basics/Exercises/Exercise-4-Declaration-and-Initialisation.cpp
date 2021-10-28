3f5036f64e9d87941b4c68d7175bf00366624ca9
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-10-28 20:20:18 +0400

    Create Exercise-4-Declaration-and-Initialisation.cpp

---
+++ Chapter 4 - C++ Basics/Exercises/Exercise-4-Declaration-and-Initialisation.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int  a = 8; // Declaring an int variable with a value of 8 as an initialsation
+    int b = 10; // Declaring an int variable with a value of 10 as an initialisation
+    int sum; // Declaring an int variable that sums the answer
+
+    sum = a + b; // Formula for the sum
+
+    cout << sum; // Outputs the sum
+
+    cin.get(); // Keeps the console window open
+    return 0;
+
+}
+
