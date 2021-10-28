55b3607f88927b8577291b3c8b18d17eddbb3c89
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-10-28 19:29:20 +0400

    Create Exercise-1-Possible-Data-Types.cpp

---
+++ Chapter 4 - C++ Basics/Exercises/Exercise-1-Possible-Data-Types.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	int firstNumber = 1; // Used int to declare whole numbers
+
+	bool iCanCode = true; // Used bool since it can only declare two variables: true and false
+
+	char hopefulGrade = 'a'; // Used character to declare only one letter and uses ' 
+
+	double myDecimal = 1.0; // Used double to declare decimals
+
+	string minimalSentence = "y"; // Used string to declare since the question uses "
+
+	int keyMash = 13213123; // Used int to declare whole numbers 
+
+	float mysteryDataType = 5.6f; // Used float to declare floating decimal points
+
+	cin.get(); // Keeps the console window open
+
+	return 0;
+
+
+	}
