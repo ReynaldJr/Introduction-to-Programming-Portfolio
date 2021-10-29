7d03ff65f0449ae42c9f718f497812f29d0564f3
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-10-29 14:31:00 +0400

    Fix a grammatical error in the output message

--- Chapter 5 - Exercises/Exercise-2-Temperature-Fahrenheit-to-Celsius.cpp
+++ Chapter 5 - Exercises/Exercise-2-Temperature-Fahrenheit-to-Celsius.cpp
@@ -6,7 +6,7 @@ int main() {
 
 	double F, C; // Declaring variables for Fahrenheit and Celsius
 
-	cout << "Please enter the a temperature in Fahrenheit that will be converted to Celsius" << endl; // Asks the user for input
+	cout << "Please enter a temperature in Fahrenheit that will be converted to Celsius" << endl; // Asks the user for input
 	cin >> F; // User inputs the temperature in Fahrenheit
 
 	C = (F - 32) * (0.556); // Formula to convert from Fahrenheit to Celsius
