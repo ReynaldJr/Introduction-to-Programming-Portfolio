c222bce28478f7163c2b534dc7a530c710bafbe4
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-10-30 22:55:43 +0400

    Add comments to make user understand the code better

--- Chapter 9 - While & Do-While Loops/Exercises/Exercise-1-Reverse-9-Times-Table.cpp
+++ Chapter 9 - While & Do-While Loops/Exercises/Exercise-1-Reverse-9-Times-Table.cpp
@@ -3,18 +3,18 @@ using namespace std;
 
 int main()
 {
-    int num = 108;
+    int num = 108; // Declaring and initialising the variable for num
 
-    while (num >= 9) {
+    while (num >= 9) { // While conditional check
 
-        cout << num << "\n";
+        cout << num << "\n"; // Outputs the num 
 
-        num = num - 9;
+        num = num - 9; // Minuses the num by 9 to create a reverse 9 times table with a loop
 
         
     }
 
-    cin.get();
+    cin.get(); // Keeps the console window open
     return 0;
 
 }
