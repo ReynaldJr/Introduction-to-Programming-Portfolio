903b9de145ee959d0c746af9a45b02f3e2b2ad02
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-10-30 23:41:24 +0400

    Create Exercise-3-Input-Improvement.cpp

---
+++ Chapter 9 - While & Do-While Loops/Exercises/Exercise-3-Input-Improvement.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	char input; // Declaring variable for user input
+
+	do { 
+
+		cout << "Would you like to Quit (Y/N)?\n"; // Instructions for user input
+		cin >> input; // User inputs their answer
+
+
+	} while ((input != 'Y') && (input != 'y')); // Improved the code here to allow the program to read lowercase y. 
+
+	return 0;
+}
