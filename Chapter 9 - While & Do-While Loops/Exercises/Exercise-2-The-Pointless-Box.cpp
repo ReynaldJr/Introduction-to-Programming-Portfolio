97b10cca111a72d8fc7f8faa19db6a82a311aad1
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-10-30 23:28:42 +0400

    Create Exercise-2-The-Pointless-Box.cpp

---
+++ Chapter 9 - While & Do-While Loops/Exercises/Exercise-2-The-Pointless-Box.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    cout << "Enter a number from 1-2\n";
+    cin >> number;
+
+    while (number == 1 || number == 2 ) {
+
+        if (number == 1) {
+
+            cout << "You have entered the number 1\n";
+        }
+
+        else {
+            cout << "You have entered the number 2\n";
+        }
+
+        
+    }
+
+}
+
