d10fa85cc3f5aaaf965d0797d23faa86a9924c26
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-10-30 22:52:45 +0400

    Create Exercise-1-Reverse-9-Times-Table.cpp

---
+++ Chapter 9 - While & Do-While Loops/Exercises/Exercise-1-Reverse-9-Times-Table.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 108;
+
+    while (num >= 9) {
+
+        cout << num << "\n";
+
+        num = num - 9;
+
+        
+    }
+
+    cin.get();
+    return 0;
+
+}
+
