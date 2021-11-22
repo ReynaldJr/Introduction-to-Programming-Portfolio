00198825e7186689d84177f2d35a218aa801dc10
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-11-23 00:01:11 +0400

    Change program to follow the instructions clearly

--- Chapter-12-Arrays/Exercises/Exercise-1-Months-of-the-Year.cpp
+++ Chapter-12-Arrays/Exercises/Exercise-1-Months-of-the-Year.cpp
@@ -5,11 +5,5 @@ int main()
 {
 	string months[12] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
-	for (int a = 0; a < 12; a++) {
-
-		cout << months[a] << endl;
-	}
-
-
 }
 
