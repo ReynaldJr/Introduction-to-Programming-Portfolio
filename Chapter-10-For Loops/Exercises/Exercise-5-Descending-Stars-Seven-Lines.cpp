0208157ee6db60957607f8690d78d12595fec766
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-11-26 11:41:07 +0400

    Add comments to make the user understand the program better

--- Chapter-10-For Loops/Exercises/Exercise-5-Descending-Stars-Seven-Lines.cpp
+++ Chapter-10-For Loops/Exercises/Exercise-5-Descending-Stars-Seven-Lines.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 int main()
 {
-    for (int i = 1; i <= 7; i++) {
-        for (int j = i; j <= 7; j++) {
-            cout << "*";
+    for (int i = 1; i <= 7; i++) { // Executes the outer loop 7 times
+        for (int j = i; j <= 7; j++) { // Executes the inner loop 7 times
+            cout << "*"; // Print reducing the lines of stars
         }
-        cout << endl;
+        cout << endl; // Print a line break to the console
     }
+
+    cin.get(); // Keeps the console window open
+    return 0;
 }
-
-
