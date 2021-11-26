69272308457cc70d8393a8a51a5eac1e6f0ebfeb
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-11-26 12:45:09 +0400

    Add comments to make user understand the program better

--- Chapter-10-For Loops/Exercises/Exercise-6-Rising-Stars-Five-Lines.cpp
+++ Chapter-10-For Loops/Exercises/Exercise-6-Rising-Stars-Five-Lines.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 int main()
 {
-    for (int i = 1; i <= 5; i++) {
-        for (int j = 1; j <= i; j++) { 
-            cout << "*"; 
+    for (int i = 1; i <= 5; i++) { // Executes the outer loop 5 times
+        for (int j = 1; j <= i; j++) { // Executes the inner loop 5 x 5 times
+            cout << "*"; // Prints increasing lines of stars
         }
-        cout << endl; 
+        cout << endl; // Prints a line break to the console
     }
+
+    cin.get(); // Keeps the console window open
+    return 0;
 }
-
-
