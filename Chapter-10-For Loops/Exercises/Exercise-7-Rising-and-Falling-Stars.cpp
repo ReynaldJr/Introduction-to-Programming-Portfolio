78139a9b0fa2ba655f50378740b6fd01a0c55396
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-11-26 21:19:08 +0400

    Add a cin.get function to keep the console window open

--- Chapter-10-For Loops/Exercises/Exercise-7-Rising-and-Falling-Stars.cpp
+++ Chapter-10-For Loops/Exercises/Exercise-7-Rising-and-Falling-Stars.cpp
@@ -3,25 +3,21 @@ using namespace std;
 
 int main()
 {
-    for (int i = 1; i <= 5; i++) {
-        for (int j = 1; j <= i; j++) {
-            cout << "*";
+    for (int i = 1; i <= 5; i++) { // Executes the outer loop 5 times
+        for (int j = 1; j <= i; j++) { // Executes the inner loop 5 x 5 times
+            cout << "*"; // Prints increasing lines of stars
         }
-        cout << endl;
+        cout << endl; // Prints a line break to the console
     }
 
-    for (int a = 1; a <= 4; a++) {
-        
+    for (int a = 1; a < 5; a++) { // Executes the outer loop 4 times
+        for (int b = a; b < 5; b++) { // Executes the inner loop 4 times
 
-        for (int b = a; b <= 4; b++) {
-
-            cout << "*";
+            cout << "*"; // Prints decreasing lines of stars
         }
-
-
-           cout << endl;
-
+           cout << endl; // Prints a line break to the console
     }
+
+    cin.get(); // Keeps the console window open
+    return 0;
 }
-
-
