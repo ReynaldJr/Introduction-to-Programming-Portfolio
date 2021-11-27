1bfe3043b2c8494052f3ea9c93ec8475146d4ac4
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-11-27 17:54:18 +0400

    Add comments to make user understand the program better

--- Chapter-10-For Loops/Exercises/Exercise-9-Find-the-9s.cpp
+++ Chapter-10-For Loops/Exercises/Exercise-9-Find-the-9s.cpp
@@ -3,19 +3,24 @@ using namespace std;
 
 int main()
 {
-    int sum = 0;
-    cout << "The numbers from 100-200 that are divisible from 9 are:\n\n";
-    for (int a = 100; a <= 200; a++) {
+    int sum = 0; // Declaring and initializing the variable sum
 
-        if (a % 9 == 0) {
+    cout << "The numbers from 100-200 that are divisible from 9 are:\n\n"; // Prints what is going to output
 
-            cout << a << endl;
+    for (int a = 100; a <= 200; a++) { // For loop that counts from 100 - 200 in increments of 1
 
-            sum = a + sum;
+        if (a % 9 == 0) { // If the number is divisible by 9, this will execute
+
+            cout << a << endl; // Outputs the numbers that are divisible by 9
+
+            sum = a + sum; // Adds the number that is divisible by 9
 
         }
     }
 
-    cout << "\n\nThe sum of all the numbers is: ";
-    cout << endl << sum << endl;
+    cout << "\n\nThe sum of all the numbers is: "; // Prints what is going to output
+    cout << endl << sum << endl; // Prints the sum of the numbers from 100-200 that is divisible by 9
+
+    cin.get(); // Keeps the console window open
+    return 0;
 }
