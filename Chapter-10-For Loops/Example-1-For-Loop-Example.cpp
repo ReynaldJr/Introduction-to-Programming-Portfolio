a0e9778d5bc2d0a0c198bd28e4a88cbee8e50c06
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-11-27 17:58:49 +0400

    Create Example-1-For-Loop-Example.cpp

---
+++ Chapter-10-For Loops/Example-1-For-Loop-Example.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int x = 0; x < 10; x++) { // For loop that counts from 0-10 in increments of 1
+
+        cout << "X is " << x << "\n"; // Prints the for loop
+
+    }
+}
