b867454400b7f7f83a5e0072ac844ca05fe19374
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-12-02 00:17:47 +0400

    Create Exercise-1.cpp

---
+++ Chapter-14-Standard Library Arrays/Exercises/Exercises/Exercise-1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <array>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int six = 0; // Declaring the variable for counter for six
+
+	int randomArray[1000]; // Declaring an array with 1000 numbers
+
+	for (int i = 0; i < 1000; i++) { // A for loop to iterate through the 1000 numbers
+
+		randomArray[i] = rand() % 100; // Generating random variables between 1-100
+
+		cout << randomArray[i] << endl; // Prints out the random numbers
+
+		if (randomArray[i] == 6) { // If statement if the random numbers display a 6
+			six++; // Increments the counter for six
+		}
+
+	}
+
+	cout << endl << six << " 6s have appeared in the console\n"; // Prints out how many times a 6 appeared
+	cin.get(); // Keeps the console window open
+	return 0;
+}
