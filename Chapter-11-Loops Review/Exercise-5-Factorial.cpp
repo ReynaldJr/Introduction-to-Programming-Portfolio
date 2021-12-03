a27027397b5a67100fef10c3507548c48c37a98d
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-12-03 19:30:58 +0400

    Add a cin.get function to keep console window open

--- Chapter-11-Loops Review/Exercise-5-Factorial.cpp
+++ Chapter-11-Loops Review/Exercise-5-Factorial.cpp
@@ -3,18 +3,20 @@ using namespace std;
 
 int main() {
 
-	int num;
-	int factorial = 1;
+	int num; // Declaring variable for user input
+	int factorial = 1; // Declaring variable for the formula in factorial
 
-	cout << "Please input a number that you wanna see the factorial of\n";
-	cin >> num;
+	cout << "Please input a number that you wanna see the factorial of\n"; // Asks the user for input
+	cin >> num; // User inputs the number they wish to see
 
-	for (int counter = 1; counter <= num; counter++) {
+	for (int counter = 1; counter <= num; counter++) { // For loop that counts from 1 until the user input in increments of 1
 
-		factorial = factorial * counter;
+		factorial = factorial * counter; // Formula for factorial
 
 	}
 	
-	cout << "The factorial of " << num << " is " << factorial << endl;
+	cout << "The factorial of " << num << " is " << factorial << endl; // Outputs the factorial
+	cin.get(); // Keeps the console window open
+	return 0;
 
 }
