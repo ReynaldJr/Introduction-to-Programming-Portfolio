8abd3454ca739ed1700736e6fa31448761e05248
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-12-03 20:08:55 +0400

    Add a cin.fail function to show error if user enters wrong input

--- Chapter-11-Loops Review/Exercise-5-Factorial.cpp
+++ Chapter-11-Loops Review/Exercise-5-Factorial.cpp
@@ -9,6 +9,13 @@ int main() {
 	cout << "Please input a number that you wanna see the factorial of\n"; // Asks the user for input
 	cin >> num; // User inputs the number they wish to see
 
+	while (cin.fail()) { // A cin.fail function if user inputs the wrong input
+		cout << "Invalid input. Please try again\n";
+		cin.clear();
+		cin.ignore(1000, '\n');
+		cin >> num;
+	}
+
 	for (int counter = 1; counter <= num; counter++) { // For loop that counts from 1 until the user input in increments of 1
 
 		factorial = factorial * counter; // Formula for factorial
