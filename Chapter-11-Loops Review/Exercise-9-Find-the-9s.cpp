47ec88c752efb5ed6ff8d518bb7c15fe56458237
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-12-04 20:30:52 +0400

    Add a cin.get function to keep console window open

--- Chapter-11-Loops Review/Exercise-9-Find-the-9s.cpp
+++ Chapter-11-Loops Review/Exercise-9-Find-the-9s.cpp
@@ -3,21 +3,24 @@ using namespace std;
 
 int main()
 {
-    int a = 0;
-    int sum = 0;
-    cout << "The numbers between 0-200 that are divisible by 9 are\n";
+    int a = 0, sum = 0; // Declaring and initializing variable a for counting, and sum to both 0;
+    cout << "The numbers between 0-200 that are divisible by 9 are\n"; // Tells the user the things that are going to be outputted next
 
     do {
-        if (a % 9 == 0) {
 
-            cout << a << endl;
+        if (a % 9 == 0) { // If the numbers from 0-200 are divisible by 9, this will execute
 
-            sum = sum + a;
+            cout << a << endl; // Outputs the numbers that are divisible by 9
+
+            sum = sum + a; // Formula to add all the numbers divisible by 9
         }
 
-        a++;
+        a++; // Incrementing a by 1
 
-    } while (a <= 200);
+    } while (a <= 200); // While loop that executes until 200
 
-    cout << "\nThe sum of all the numbers between 0-200 that are divisible by 9 is: " << sum << endl;
+    cout << "\nThe sum of all the numbers between 0-200 that are divisible by 9 is: " << sum << endl; // Outputs the sum 
+
+    cin.get(); // Keeps the console window open
+    return 0;
 }
