5d8f20d17bd91d6c38ba7f85c69cd88a7eca0e73
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-12-18 20:12:58 +0400

    Add comments to make user understand the program better

--- Chapter-15-Functions Part 1/Exercises/Exercise-1-You-Say-Hello-I-Say-Goodbye.cpp
+++ Chapter-15-Functions Part 1/Exercises/Exercise-1-You-Say-Hello-I-Say-Goodbye.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 using namespace std;
 
-void welcome();
-void goodbye();
+void welcome(); // Declaring function
+void goodbye(); // Declaring function
 
 
 int main()
 {
-    welcome();
-    goodbye();
+    welcome(); // Calls a function
+    goodbye(); // Calls a function
+
+    cin.get(); // Keeps the console window open
     return 0;
 }
 
-void welcome() {
+void welcome() { // Defining function
 
     cout << "Welcome to my program\n";
 }
 
-void goodbye() {
+void goodbye() { // Defining function
 
     cout << "End of program\n";
 }
