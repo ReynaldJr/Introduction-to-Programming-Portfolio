57132e0a435d0a9c38085c077416a62af277cb48
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-12-18 19:43:42 +0400

    Add comments to make user understand program better

--- Chapter-15-Functions Part 2/Exercises/Exercise-1-Purchase-Products.cpp
+++ Chapter-15-Functions Part 2/Exercises/Exercise-1-Purchase-Products.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 using namespace std;
 
-double sumItems(double item1, double item2) {
+double sumItems(double item1, double item2) { // Declaring function for the sum of two items
 
-    double total = item1 + item2;
-    return total;
+    double total = item1 + item2; // Formula for summing up two items
+    return total; // Returning total to main function
 }
 int main()
 {
 
-    double myMoney = 40.00, shoes, tshirt;
-    cout << "You currently have " << myMoney << " AED in your pocket\n";
+    double myMoney = 40.00, shoes, tshirt; // Declaring variables
+    cout << "You currently have " << myMoney << " AED in your pocket\n"; // Outputs how much money the user has
 
-    cout << "How much is the shoes?\n";
-    cin >> shoes;
-    cout << "How much is the tshirt?\n";
-    cin >> tshirt;
+    cout << "How much is the shoes?\n"; // Asks user for input
+    cin >> shoes; // User inputs the price for shoes
+    cout << "How much is the t-shirt?\n"; // Asks user for input
+    cin >> tshirt; // User inputs the price for t-shirts
 
-    if (sumItems(shoes, tshirt) <= myMoney) {
-        cout << "You can afford these items\n";
+    if (sumItems(shoes, tshirt) <= myMoney) { // If statement that determines if user can afford the items
+        cout << "You can afford these items\n"; // Outputs if user can afford the items
     }
     else {
-        cout << "Keep saving up!\n";
+        cout << "Keep saving up!\n"; // Outputs if user can't afford the items
     }
+
+    cin.get(); // Keeps console window open
+    return 0;
 }
