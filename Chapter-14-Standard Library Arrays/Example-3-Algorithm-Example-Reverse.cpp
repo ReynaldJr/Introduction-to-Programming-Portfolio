b6ec1fc133ab8e39ddb769c0f97b2bd09ebcab60
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-12-18 22:53:18 +0400

    Create Example-3-Algorithm-Example-Reverse.cpp

---
+++ Chapter-14-Standard Library Arrays/Example-3-Algorithm-Example-Reverse.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <array>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	array<int, 5> numbers = {33, 5, 7, 99, 83};
+	sort(numbers.begin(), numbers.end());
+	reverse(numbers.begin(), numbers.end());
+
+	for (int num : numbers) {
+
+		cout << num << " ";
+	}
+}
