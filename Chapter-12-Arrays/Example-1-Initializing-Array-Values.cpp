6d41c6c4121d5fc0cd5e4f8aaea8628e7e5462f4
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-12-19 14:41:22 +0400

    Create Example-1-Initializing-Array-Values.cpp

---
+++ Chapter-12-Arrays/Example-1-Initializing-Array-Values.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//longhand
+	int ages[5];
+	ages[0] = 19;
+	ages[1] = 23;
+	ages[2] = 22;
+	ages[3] = 30;
+	ages[4] = 18;
+
+	//shorthand
+	int array[] = { 19, 23, 22, 30, 18 };
+
+}
