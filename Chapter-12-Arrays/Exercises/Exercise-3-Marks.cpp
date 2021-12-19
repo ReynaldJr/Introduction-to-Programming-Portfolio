e4c3da50a18349a61b2a3753c65d91f7cdd0f237
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-12-19 15:07:50 +0400

    Add a cin.get function

--- Chapter-12-Arrays/Exercises/Exercise-3-Marks.cpp
+++ Chapter-12-Arrays/Exercises/Exercise-3-Marks.cpp
@@ -3,30 +3,32 @@ using namespace std;
 
 int main()
 {
-    cout << "Grades\n";
+    cout << "Average of 5 Marks Calculator\n"; // Outputs title of program
 
-    int grades[5];
-    double sum, average;
-    sum = 0;
+    int grades[5]; // Declaring variable for user input
+    double sum = 0, average = 0; // Declaring and initializing variable for sum and average
 
 
-    for (int i = 0; i < 5; i++) {
-        cout << "Please enter the mark for subject " << i+1 << endl;
-        cin >> grades[i];
+    for (int i = 0; i < 5; i++) { // For loop that counts a variable from 0 to 4 in increments of 1
+        cout << "Please enter the mark for subject " << i+1 << endl; // Asks user for input
+        cin >> grades[i]; // User inputs mark 5 times
 
-        while (cin.fail() || grades[i] < 0 || grades[i] > 100) {
+        while (cin.fail() || grades[i] < 0 || grades[i] > 100) { // An error will be shown if user inputs anything that isn't a number or if the number is a negative or more than 100
             cout << "Invalid input, please try again!\n";
             cin.clear();
             cin.ignore(1000, '\n');
             cin >> grades[i];
             }
-        sum = sum + grades[i];
+        sum = sum + grades[i]; // Formula for sum
 
 
     }
 
-    average = sum / 5;
+    average = sum / 5; // Formula for average
 
-    cout << "Your average is " << average << endl;
+    cout << "Your average is " << average << endl; // Outputs the average
+
+    cin.get(); // Keeps the console window open
+    return 0;
 
 }
