baebb6dc5dc837044acec8165c5c16a97cc79cbc
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-12-19 18:52:40 +0400

    Create Example-1-Statements.cpp

---
+++ Chapter 6 - IF Statements/Example-1-Statements.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// IF Statement
+
+	int x = 9;
+	if (x == 7) {
+		cout << "x IS equal to 7" << endl;
+	}
+
+	// IF-ELSE Statement
+
+	int y = 9;
+	if (y == 7)
+	{
+		cout << "y IS equal to 7" << endl;
+	}
+	else
+	{
+	cout << "y IS NOT equal to 7" << endl;
+	}
+
+}
