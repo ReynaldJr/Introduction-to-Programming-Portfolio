210cfedb8e2719e429dc0f469999ae4f8992fe7d
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-12-19 20:28:56 +0400

    Create Example-1-Standard-Math-Library.cpp

---
+++ Extra Lessons/Standard Math Library/Example-1-Standard-Math-Library.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    cout << "The cube of 8 is " << pow(8, 3) << endl; // pow (number, exponent value)
+
+    cout << "The square-root of 8 is " << sqrt(8) << endl;
+    cout << "The square-root of 8 is " << pow(8,0.5) << endl; // pow (number, exponent value)
+
+
+    cout << "The cube-root of 8 is " << cbrt(8) << endl;
+    cout << "The cube-root of 8 is " << pow(8, 0.33333333333) << endl; // pow (number, exponent value)
+
+}
