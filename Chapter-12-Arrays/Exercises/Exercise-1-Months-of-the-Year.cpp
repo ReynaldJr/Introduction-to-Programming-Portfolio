7deff0f68160bf413b39ff8c61b2950351b8aae3
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-12-19 14:45:26 +0400

    Add comments

--- Chapter-12-Arrays/Exercises/Exercise-1-Months-of-the-Year.cpp
+++ Chapter-12-Arrays/Exercises/Exercise-1-Months-of-the-Year.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 int main()
 {
-	string months[12] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+	string months[12] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}; // Declaring and initializing an array that stores the values for each month of the year
 
 }
-
