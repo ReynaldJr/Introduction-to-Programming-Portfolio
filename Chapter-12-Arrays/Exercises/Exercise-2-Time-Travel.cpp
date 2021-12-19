262cb7f4d0cf63beec271bebebcbe150179fd7a2
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-12-19 14:46:59 +0400

    Reduce lines of code

--- Chapter-12-Arrays/Exercises/Exercise-2-Time-Travel.cpp
+++ Chapter-12-Arrays/Exercises/Exercise-2-Time-Travel.cpp
@@ -3,15 +3,13 @@ using namespace std;
 
 int main()
 {
-	string months[12] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+	string months[12] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" }; // Declaring and initializing an array that stores the values for each month of the year
 
-	for (int i = 0; i < 12; i++) {
+	for (int i = 0; i < 12; i++) { // A for loop that outputs the values stored in the array
 
 		cout << months[i] << endl;
 	}
 
-
-
-
+	cin.get(); // Keeps console window open
+	return 0;
 }
-
