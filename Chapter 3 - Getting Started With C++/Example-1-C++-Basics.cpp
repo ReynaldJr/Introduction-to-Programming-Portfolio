08a3a92865ec26114d622500ac253eabf670fba3
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-12-19 20:02:02 +0400

    Create Example-1-C++-Basics.cpp

---
+++ Chapter 3 - Getting Started With C++/Example-1-C++-Basics.cpp
@@ -0,0 +1,23 @@
+#include <iostream> // preprocessor directive - include iostream file 
+
+using namespace std; // declare use of the standard namespace
+
+int main() { // define main function
+	cout << "Hello World" << endl; // output statement
+
+	// Coding Conventions: Variable Names
+
+	string userName = "Alpha";
+	int studentID = 12;
+	string studentName = "beta";
+	int a = 12;
+
+	return 0; // return statement
+}
+
+// This is a single line comment
+
+/* This is a block or multi-line comment
+It is different to a single line
+comment Because it has multiple lines
+*/
