48c1d2014a3f9fe7f9c038952bb23889675b9581
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-12-19 17:26:01 +0400

    Add a cin.fail function

--- Chapter 9 - While & Do-While Loops/Exercises/Exercise-7-Loopy.cpp
+++ Chapter 9 - While & Do-While Loops/Exercises/Exercise-7-Loopy.cpp
@@ -10,12 +10,19 @@ int main() {
 
 	cin >> myInt; // User inputs their number
 
+	while (cin.fail()) { // A cin.fail function to show error if user inputs anything other than a number
+		cout << "Invalid input, please try again\n";
+		cin.clear();
+		cin.ignore(1000, '\n');
+		cin >> myInt;
+	}
+
 	do {
 		cout << counter << endl; // Outputs the number
 		counter++; // Incrementing the number
 
 	} while (counter <= myInt); // Loops until the counter reached the number of the user input
 
-    cin.get(); // Keeps console window open
+	cin.get(); // Keeps console window open
 	return 0;
 }
