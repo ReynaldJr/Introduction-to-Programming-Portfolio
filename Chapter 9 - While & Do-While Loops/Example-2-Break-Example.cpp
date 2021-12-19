647c7b04f5a8a7d2b5dc9ae36e1236fee25c589f
ReynaldJr <91404670+ReynaldJr@users.noreply.github.com>
2021-12-19 17:06:52 +0400

    Create Example-2-Break-Example.cpp

---
+++ Chapter 9 - While & Do-While Loops/Example-2-Break-Example.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Enter a whole number: " << endl;
+	int userNum;
+	cin >> userNum;
+
+	int count = 1;
+
+	while (count <= userNum) {
+	cout << count << endl;
+		
+	count++;
+
+	}
+
+}
